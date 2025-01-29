cc16df97a25d10c0659165f16919695593413ddd
kimchijuice <105619425+kimchijuice@users.noreply.github.com>
2025-01-29 10:15:16 +0800

    Add clear button function and click to toggle hover effect

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+// create 16 x 16 grid
 const container = document.querySelector('#container');
 
 for (let i = 0; i < 257; i++)  {
     const div = document.createElement('div');
     div.classList.add('grid-item');
-    container.appendChild(div)
+    container.appendChild(div);
 }
+
+// hover effect
+const gridItems = document.querySelectorAll('.grid-item');
+let isDrawing = false;
+
+gridItems.forEach(item => {
+    item.addEventListener('click', () => {
+        isDrawing = !isDrawing; // Toggle drawing state
+        item.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+    });
+
+    item.addEventListener('mouseover', () => {
+        if (isDrawing) {
+            item.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+        }
+    });
+});
+  
+
+// clear grid
+
+const clearButton = document.querySelector('#clearButton');
+
+clearButton.addEventListener('click', () => {
+    gridItems.forEach( item => {
+        item.style.backgroundColor = 'transparent';
+    });
+        
+})
